35798e5a84f9e3c2c2caf908913d8c402dd0d6b7
Juan Manuel Boudry Leon <buleboudry117@gmail.com>
2021-03-10 00:15:35 +0100

    Tipos enteros y tipo String en java

--- fundamentos/holaMundoJava/src/HolaMundo.java
+++ fundamentos/holaMundoJava/src/HolaMundo.java
@@ -3,7 +3,21 @@ public class HolaMundo {
     public static void main(String args[]) {
         //System.out.println("Hola mundo desde java");
         
+        //definimos la variable
         int miVariableEntera = 10;
         System.out.println(miVariableEntera);
+        
+        //modificamos el valor de la variable
+        miVariableEntera = 5;
+        //si pones sout y le das al tab te autocompleta
+        System.out.println(miVariableEntera);
+        
+        //String va con la S mayuscula ojo
+        String miVariableCadena = "Saludos";
+        //para que te autocomplete variables haces control espacio
+        System.out.println(miVariableCadena);
+        miVariableCadena = "adios";
+        //si haces control click a una variable te lleva a donde está definida
+        System.out.println(miVariableCadena);
     }
 }
