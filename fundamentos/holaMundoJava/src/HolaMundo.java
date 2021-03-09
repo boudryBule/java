6d0ae911130b0e22fd1f4f7730e0124bcd4994db
Juan Manuel Boudry Leon <buleboudry117@gmail.com>
2021-03-10 00:05:05 +0100

    variables en java

--- fundamentos/holaMundoJava/src/HolaMundo.java
+++ fundamentos/holaMundoJava/src/HolaMundo.java
@@ -1,6 +1,9 @@
 //comentario simple
 public class HolaMundo {
     public static void main(String args[]) {
-        System.out.println("Hola mundo");
+        //System.out.println("Hola mundo desde java");
+        
+        int miVariableEntera = 10;
+        System.out.println(miVariableEntera);
     }
 }
