1c76cb6274097e6fee156385f7df286c01c62707
Juan Manuel Boudry Leon <buleboudry117@gmail.com>
2021-03-10 13:14:00 +0100

    inferencia de tipos con var y tipos primitivos

---
+++ fundamentos/inferenciaVar/src/inferenciavar/InferenciaVar.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inferenciavar;
+
+/**
+ *
+ * @author buleb
+ */
+public class InferenciaVar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       var numeroEntero = 10;
+       System.out.println("numeroEntero = " + numeroEntero);
+        
+       
+       var numeroDouble = 10.0;
+       System.out.println("numeroDouble = " + numeroDouble);
+       
+       
+       
+       var numeroFloat = 10.0F;
+        System.out.println("numeroFloat = " + numeroFloat);
+        
+    }
+    
+}
