3f33f9f06c4ab3da0bdba1609a1a45f1c11818d1
Juan Manuel Boudry Leon <buleboudry117@gmail.com>
2021-03-10 12:25:10 +0100

    tipos numéricos enteros

---
+++ fundamentos/aprendiendoJava/src/aprendiendojava/AprendiendoJava.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aprendiendojava;
+
+/**
+ *
+ * @author buleb
+ */
+public class AprendiendoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        short numeroShort = (short) 32768; //lo forzamos a que sea short 
+        System.out.println("numeroShort = " + numeroShort);
+        System.out.println("Valor minimo short: " + Short.MIN_VALUE);
+        System.out.println("Valor maximo short: " + Short.MAX_VALUE);
+        
+        int numeroInt = 10;
+        System.out.println("numeroInt = " + numeroInt);
+        System.out.println("Valor minimo int: " + Integer.MIN_VALUE);
+        System.out.println("Valor maximo int: " + Integer.MAX_VALUE);
+        
+        long NumeroLong = 9223372036854775807L; //si quieres poner el numero completo tienes que ponerle una L al final para que sepa que es un long
+        System.out.println("numeroLong = " + NumeroLong);
+        System.out.println("Valor minimo long: " + Long.MIN_VALUE);
+        System.out.println("Valor maximo long: " + Long.MAX_VALUE);
+        
+    }
+    
+}
