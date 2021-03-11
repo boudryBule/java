4aeeae31dcbe54fc3690a15e9d3db7772d9a5f2a
Juan Manuel Boudry Leon <buleboudry117@gmail.com>
2021-03-11 12:54:23 +0100

    sentencia de control if

---
+++ fundamentos/ifelse/src/ifelse/Ifelse.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ifelse;
+
+/**
+ *
+ * @author buleb
+ */
+public class Ifelse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        var condicion = true;
+        if(condicion){
+            System.out.println("Condicion verdadera");
+        }else{
+            System.out.println("Condicion falsa");
+        }
+    }
+    
+}
