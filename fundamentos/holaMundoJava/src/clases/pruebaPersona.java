a58ff26274eb7587de32824987fa3bd9df0d5f24
Juan Manuel Boudry Leon <buleboudry117@gmail.com>
2021-03-12 10:39:31 +0100

    objetos2

--- fundamentos/holaMundoJava/src/clases/pruebaPersona.java
+++ fundamentos/holaMundoJava/src/clases/pruebaPersona.java
@@ -5,10 +5,19 @@ package clases;
 public class pruebaPersona {
     //psvm y te escribe lo del main
     public static void main(String[] args) {
-        Persona persona1;
-        persona1 = new Persona();
+        Persona persona1= new Persona();
         persona1.nombre = "Juan";
         persona1.apellido = "Perez";
         persona1.desplegarInformacion();
+        
+        
+        
+        Persona persona2 = new Persona();
+        System.out.println("persona2 = " + persona2);
+        
+        persona2.desplegarInformacion();
+        persona2.nombre = "Pepe";
+        persona2.apellido ="Viyuela";
+        persona2.desplegarInformacion();
     }
 }
