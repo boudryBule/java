a2bce18b073abfb83ebf6c85027720442eed037e
Juan Manuel Boudry Leon <buleboudry117@gmail.com>
2021-03-12 12:50:59 +0100

    operador this

--- fundamentos/aritmetica/src/operaciones/Aritmetica.java
+++ fundamentos/aritmetica/src/operaciones/Aritmetica.java
@@ -16,12 +16,19 @@ public class Aritmetica {
 //        int resultado = a + b;
 //        return resultado;
         return a + b;
-    }
+    } 
     
-    public int sumarConArgumentos(int arg1, int arg2) { //aqui son argumentos
-        a = arg1;
-        b = arg2;        
+//    public int sumarConArgumentos(int arg1, int arg2) { //aqui son argumentos
+//        a = arg1;
+//        b = arg2;        
+////        return a + b;
+//        return sumarConRetorno();
+    
+    //this hace referencia al atributo de nuestra clase que se está ejecutando en ese justo momento
+        public int sumarConArgumentos(int a, int b) { //aqui son argumentos
+        this.a = a; //el argumento a se le asigna al atributo this.a
+        this.b = b;        
 //        return a + b;
-        return sumarConRetorno();
+        return this.sumarConRetorno();
     }
 }
