f4d181d08e8d7447164b5197a343279d77b100d8
Juan Manuel Boudry Leon <buleboudry117@gmail.com>
2021-03-13 17:53:09 +0100

    uso de la palabra return y null

--- fundamentos/holaMundoJava/src/pasoporreferencia/PasoPorReferencia.java
+++ fundamentos/holaMundoJava/src/pasoporreferencia/PasoPorReferencia.java
@@ -8,11 +8,15 @@ public class PasoPorReferencia {
         Persona persona1 = new Persona();
         persona1.nombre = "Juan Alberto";
         System.out.println("persona1 = " + persona1.nombre);
-        cambiarValor(persona1);
-        System.out.println("persona1 = " + persona1.nombre);
+        persona1 = cambiarValor(persona1);
+        System.out.println("persona1 = " + persona1.nombre );
     }
     
-    public static void cambiarValor(Persona persona) {
+    public static Persona cambiarValor(Persona persona) {
         persona.nombre = "Karla";
+    
+        return persona;
     }
 }
+
+   
