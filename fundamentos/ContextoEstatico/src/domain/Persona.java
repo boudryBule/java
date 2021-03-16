979a21c3fccf67f598dd0e63ab4e02314c9e58c2
Juan Manuel Boudry Leon <buleboudry117@gmail.com>
2021-03-16 18:46:20 +0100

    Contexto estático en java parte 2

--- fundamentos/ContextoEstatico/src/domain/Persona.java
+++ fundamentos/ContextoEstatico/src/domain/Persona.java
@@ -3,13 +3,7 @@ package domain;
 
 public class Persona {
 
-    public static int getContadorPersonas() {
-        return contadorPersonas;
-    }
 
-    public static void setContadorPersonas(int aContadorPersonas) {
-        contadorPersonas = aContadorPersonas;
-    }
     private int idPersona;
     private String nombre;
     private static int contadorPersonas; //se ira incrementando con cada objeto que se cree
@@ -20,7 +14,7 @@ public class Persona {
         this.nombre  = nombre;
         //incrementamos el contador por cada objeto nuevo
         //this.contadorPersonas++; al ser static no se recomienda usar this
-        Persona.contadorPersonas++;
+        Persona.contadorPersonas++; //al ser static es bueno que pongamos Persona.
         //Asignar el nuevo valor a la variable idPersona
         this.idPersona = Persona.contadorPersonas;
     }
@@ -39,6 +33,19 @@ public class Persona {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+    
+    public static int getContadorPersonas() {
+        return contadorPersonas;
+    }
+
+    public static void setContadorPersonas(int aContadorPersonas) {
+        contadorPersonas = aContadorPersonas;
+    }
+
+    @Override //esta nota está indicando que estamos sobreescribiendo el metodo tostring
+    public String toString() {
+        return "Persona{" + "idPersona=" + idPersona + ", nombre=" + nombre + '}';
     }
     
     
---
+++ fundamentos/ContextoEstatico/src/test/PersonaPrueba.java
@@ -0,0 +1,35 @@
+
+package test;
+
+import domain.Persona;
+
+public class PersonaPrueba {
+    private int contador;
+
+    public static void main(String[] args) {
+       
+        
+        Persona persona1 = new Persona("Juan");
+        
+//        System.out.println("persona1 = " + persona1);
+        
+        Persona persona2 = new Persona("Karla");
+//        System.out.println("persona2 = " + persona2);
+        //el contador se va incrementando ya que es static, si no lo fuese todos saldrían con id= 1
+        imprimir(persona1);
+        imprimir(persona2);
+        
+       //this.contador = 10; variable no estatica no puede ser referenciada desde un estatico
+       
+    }
+    
+    //el metodo imprimir debe de ser estatico, ya que si es dinamico se asocia a los objetos que se vayan a crear en la clase (Personaprueba que no crea nada) por lo que da error
+    public static void imprimir( Persona persona){
+        System.out.println("persona = " + persona);
+    }
+    
+    public int getContador() {
+        imprimir(new Persona("Carlos"));
+        return this.contador;
+    }
+}
