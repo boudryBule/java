c68682e6227a1ffa0374759d6d8c774a5ed3b636
Juan Manuel Boudry Leon <buleboudry117@gmail.com>
2021-03-17 18:49:04 +0100

    Herencia 3

--- fundamentos/Herencia/src/domain/Empleado.java
+++ fundamentos/Herencia/src/domain/Empleado.java
@@ -1,5 +1,45 @@
 package domain;
 
 public class Empleado extends Persona { //así indicas que hereda de Persona
+      private int idEmpleado;
+      private double sueldo;
+      private static int contadorEmpleado;
+
+    public Empleado(String nombre, double sueldo) {
+        super(nombre); //para poner un atributo de una clase de la que heredas tienes que usar super
+        this.idEmpleado = ++Empleado.contadorEmpleado; //le ponemos el Empleado. para saber que es una variable estática
+        this.sueldo = sueldo;
+    }
+
+    public int getIdEmpleado() {
+        return this.idEmpleado;
+    }
+    
+    
+///  el metodo set de idempleado no lo vamos a usar asi que lo quitamos ya que el constructor es el que se va a encargar de asignar este valor
+//    public void setIdEmpleado(int idEmpleado) {
+//        this.idEmpleado = idEmpleado;
+//    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    //este es el metodo tostring cuando le das a que use stringbuilder
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Empleado{idEmpleado=").append(this.idEmpleado);
+        sb.append(", sueldo=").append(this.sueldo);
+        sb.append(", ").append(super.toString());
+        sb.append('}');
+        return sb.toString();
+    }
+      
+    
       
 }
--- fundamentos/Herencia/src/test/Test.java
+++ fundamentos/Herencia/src/test/Test.java
@@ -4,7 +4,7 @@ import domain.Empleado;
 
 public class Test {
     public static void main(String[] args) {
-        Empleado empleado1 = new Empleado();
+        Empleado empleado1 = new Empleado("Juan", 5000.0);
         System.out.println("empleado1 = " + empleado1); //aunque no tenemos nada en la clase empleado, al heredar ya tenemos todos los atributos de la clase  Persona
     }
 }
