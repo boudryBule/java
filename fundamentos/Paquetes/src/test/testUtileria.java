c782dac2429557f5a8d3bd868996932675718413
Juan Manuel Boudry Leon <buleboudry117@gmail.com>
2021-03-18 18:30:17 +0100

    distribución de paquetes en java

---
+++ fundamentos/Paquetes/src/test/testUtileria.java
@@ -0,0 +1,12 @@
+package test;
+//import mx.com.globalmentoring.Utileria;
+//import mx.com.globalmentoring.*; //al poner el * estás importando todas las clases que haya en ese paquete
+//import static mx.com.globalmentoring.Utileria.imprimir;
+
+public class testUtileria {
+    public static void main(String[] args) {
+//        Utileria.imprimir("Saludos");
+//    imprimir("adios");
+       mx.com.globalmentoring.Utileria.imprimir("adios");
+    }
+}
