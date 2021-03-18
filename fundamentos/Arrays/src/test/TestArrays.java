cfb22d406847fdbca87cd19dcbae8234f5dc8ba6
Juan Manuel Boudry Leon <buleboudry117@gmail.com>
2021-03-18 20:32:20 +0100

    Modificar valores de los arrays

--- fundamentos/Arrays/src/test/TestArrays.java
+++ fundamentos/Arrays/src/test/TestArrays.java
@@ -6,5 +6,9 @@ public class TestArrays {
     public static void main(String[] args) {
         int edades [] = new int[3];
         System.out.println("edades = " + edades);
+        
+        //vamos a acceder a cada elemento del array para modificarlo
+        edades[0] = 10;
+        System.out.println("edades 0  = " +  edades[0] );
     }
 }
