e5c2abab36686186b75fd2bc7ce0071dfb583972
Juan Manuel Boudry Leon <buleboudry117@gmail.com>
2021-03-18 20:19:10 +0100

    Arrays

---
+++ fundamentos/Arrays/src/test/TestArrays.java
@@ -0,0 +1,10 @@
+
+package test;
+
+
+public class TestArrays {
+    public static void main(String[] args) {
+        int edades [] = new int[3];
+        System.out.println("edades = " + edades);
+    }
+}
