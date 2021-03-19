1644486695adfeaff06507a6e5c9f480b55fcaba
Juan Manuel Boudry Leon <buleboudry117@gmail.com>
2021-03-19 18:32:33 +0100

    Recorrer un array

--- fundamentos/Arrays/src/test/TestArrays.java
+++ fundamentos/Arrays/src/test/TestArrays.java
@@ -10,5 +10,11 @@ public class TestArrays {
         //vamos a acceder a cada elemento del array para modificarlo
         edades[0] = 10;
         System.out.println("edades 0  = " +  edades[0] );
+        
+       //edades[3] = 5; da error de ejucion pero no de compilacion, no podemos acceder a un valor del array que esta fuera del dominio
+         
+       for(int i = 0;i< edades.length; i++) {
+           System.out.println("Edades elemento" + i + ": " + edades[i]);
+       }
     }
 }
