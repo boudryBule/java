1c85f0339644a4fa51e3f3ee148ede3179f586e9
Juan Manuel Boudry Leon <buleboudry117@gmail.com>
2021-03-23 20:09:12 +0100

    Imprimir enumeraciones con atributos

--- Programacion/Enumeraciones/src/test/TestEnumeraciones.java
+++ Programacion/Enumeraciones/src/test/TestEnumeraciones.java
@@ -1,12 +1,19 @@
 package test;
 
+import enumeracion.Continentes;
 import enumeracion.Dias;
 
 public class TestEnumeraciones {
     public static void main(String[] args) {
-        System.out.println("Dia 1: " + Dias.LUNES);
-        indicarDiaSemana(Dias.LUNES);
+//        System.out.println("Dia 1: " + Dias.LUNES);
+//        indicarDiaSemana(Dias.LUNES);
         //indicarDiaSemana("LUNES"); imcompatible pasar un string como una enumeracion de dias
+        
+        System.out.println("Continente no. 4: " + Continentes.AMERICA);
+        System.out.println("Numero de paises del cuarto continente: " + Continentes.AMERICA.getPaises());
+        
+        System.out.println("Continente no. 1: " + Continentes.AFRICA);
+        System.out.println("Numero de paises del primer continente: " + Continentes.AFRICA.getPaises());
     }
     
     private static void indicarDiaSemana(Dias dias){
