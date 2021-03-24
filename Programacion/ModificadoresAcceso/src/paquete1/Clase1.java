8dfb344d80ad3f9afacf6ca06b2841cf7b63a537
Juan Manuel Boudry Leon <buleboudry117@gmail.com>
2021-03-24 19:30:10 +0100

    Modificador de acceso protected

--- Programacion/ModificadoresAcceso/src/paquete1/Clase1.java
+++ Programacion/ModificadoresAcceso/src/paquete1/Clase1.java
@@ -2,13 +2,28 @@ package paquete1;
 
 
 public class Clase1 {
-    public String atributoPublico = "Valor atributo publico";
+//    public String atributoPublico = "Valor atributo publico";
+//    
+//    public Clase1(){
+//        System.out.println("Constructor publico");
+//    }
+//    
+//    public void metodoPublico() {
+//        System.out.println("Metodo publico");
+//    }
     
-    public Clase1(){
+    
+    //es recomendable que los marquemos como protected si los vamos a heredar a las clases hijas y si no queremos heredar lo ponemos como privado
+    protected String atributoProtected = "Valor atributo protected";
+    protected Clase1(){ //solo lo podemos usar desde clases hijas
+        System.out.println("Constructor protected");
+    }
+    
+    public Clase1(String arg) {
         System.out.println("Constructor publico");
     }
     
-    public void metodoPublico() {
-        System.out.println("Metodo publico");
+    protected void metodoProtected() {
+        System.out.println("Metodo protected");
     }
 }
---
+++ Programacion/ModificadoresAcceso/src/paquete2/ClaseHija.java
@@ -0,0 +1,14 @@
+package paquete2;
+
+import paquete1.Clase1;
+
+
+public class ClaseHija extends Clase1 {
+    
+    public ClaseHija() {
+        super();
+        this.atributoProtected = "Modificacion atributo protected";
+        System.out.println("atributoProtected = " + this.atributoProtected);
+        this.metodoProtected();
+    }
+}
