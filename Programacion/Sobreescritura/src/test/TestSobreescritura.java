a086bef5e4d9c50c030286764a26d2ee000e3189
Juan Manuel Boudry León <buleboudry117@gmail.com>
2021-03-25 20:25:46 +0100

    polimofirsmo

--- Programacion/Sobreescritura/src/test/TestSobreescritura.java
+++ Programacion/Sobreescritura/src/test/TestSobreescritura.java
@@ -1,11 +1,21 @@
 package test;
 
-import domain.Gerente;
+import domain.*;
 
 
 public class TestSobreescritura {
     public static void main(String[] args) {
-        Gerente gerente1 = new Gerente("Juan", 5000, "Sistemas");
-        System.out.println("gerente1 = " + gerente1.obtenerDetalles());
+        Empleado empleado = new Empleado("Juan", 5000);
+//        System.out.println("empleado = " + empleado.obtenerDetalles());
+        imprimir(empleado);
+        
+        Gerente gerente = new Gerente("Karla", 10000, "Contabilidad");
+        imprimir(gerente);
+//        System.out.println("gerente = " + gerente.obtenerDetalles());
+    }
+    
+    
+    public static void imprimir(Empleado empleado){
+        System.out.println("empleado = " + empleado.obtenerDetalles()); //en tiempo de ejecucion el metodo que se va a ejecutar va a ser del tipo de la referencia que reciba
     }
 }
