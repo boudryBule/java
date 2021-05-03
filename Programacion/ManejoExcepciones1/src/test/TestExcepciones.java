178cbd5beb51fb7cd81d578f40ffe9ea85fcf882
Juan Manuel Boudry León <buleboudry117@gmail.com>
2021-05-03 18:43:55 +0200

    division con excepción

--- Programacion/ManejoExcepciones1/src/test/TestExcepciones.java
+++ Programacion/ManejoExcepciones1/src/test/TestExcepciones.java
@@ -1,15 +1,19 @@
 
 package test;
 
+import static aritmetica.Aritmetica.division;
+
 public class TestExcepciones {
     public static void main(String[] args) {
         
         int resultado = 0;
         
         try{
-            resultado = 10/0;
+            resultado = division(10, 0);
         }catch(Exception e) {
+            System.out.println("Ocurrió un ");
             e.printStackTrace(System.out);//imprimimos la pila de excepciones
+            System.out.println(e.getMessage()); //asi obtendríamos solo el mensaje de la excepcion sin ver toda la pila de excepciones
         }
             System.out.println("resultado = " + resultado);
     }
