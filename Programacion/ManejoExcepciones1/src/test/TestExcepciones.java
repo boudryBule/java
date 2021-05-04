1d0ac29b4b6c1819403aa386a9788bff04582a5a
Juan Manuel Boudry León <buleboudry117@gmail.com>
2021-05-04 10:34:08 +0200

    Más excepciones

--- Programacion/ManejoExcepciones1/src/test/TestExcepciones.java
+++ Programacion/ManejoExcepciones1/src/test/TestExcepciones.java
@@ -2,19 +2,28 @@
 package test;
 
 import static aritmetica.Aritmetica.division;
+import excepciones.OperacionExcepcion;
 
 public class TestExcepciones {
     public static void main(String[] args) {
         
         int resultado = 0;
         
-        //Atry{
+        try{
             resultado = division(10, 0);
-//        }catch(Exception e) {
-//            System.out.println("Ocurrió un ");
-//            e.printStackTrace(System.out);//imprimimos la pila de excepciones
-//            System.out.println(e.getMessage()); //asi obtendríamos solo el mensaje de la excepcion sin ver toda la pila de excepciones
-//        }
+        
+        }catch (OperacionExcepcion e){
+            System.out.println("Ocurrió un error de tipo OperacionExcepcion");
+            System.out.println(e.getMessage());
+          
+        }catch(Exception e) {
+            System.out.println("Ocurrió un error de tipo Exception ");
+            e.printStackTrace(System.out);//imprimimos la pila de excepciones
+            System.out.println(e.getMessage()); //asi obtendríamos solo el mensaje de la excepcion sin ver toda la pila de excepciones
+        }
+        finally{ //finally hace que se ejecute esta parte si o si haya o no una excepcion
+            System.out.println("Se revisó la división entre cero");
+        }
             System.out.println("resultado = " + resultado);
     }
 }
