b6506a61ab922fff7117a5fb0c10d8edf84b5e89
Juan Manuel Boudry León <buleboudry117@gmail.com>
2021-05-04 19:35:15 +0200

    Generic en map y en set

--- Programacion/ApiColeccionesGenericas/src/test/TestColeccionesGenericas.java
+++ Programacion/ApiColeccionesGenericas/src/test/TestColeccionesGenericas.java
@@ -18,7 +18,7 @@ public class TestColeccionesGenericas {
        // System.out.println("elemento = " + elemento);
 
         
-        imprimir(miLista);
+//        imprimir(miLista);
         
 //           for(Object elemento: miLista) {
 //            System.out.println("elemento = " + elemento);
@@ -33,7 +33,7 @@ public class TestColeccionesGenericas {
 
         //set no añade de forma ordenada, por lo cual es más rápido 
         //en set no se permiten elementos duplicados, por lo que si repetimos un elemento, este no se volverá a mostrar 
-        Set miSet = new HashSet();
+        Set<String> miSet = new HashSet<>(); //set en generic
         
         miSet.add("lunes");
         miSet.add("Martes");
@@ -41,26 +41,27 @@ public class TestColeccionesGenericas {
         miSet.add("Jueves");
         miSet.add("Viernes");
         
-      //  imprimir(miSet);
+//        imprimir(miSet);
         
       
-      Map miMapa = new HashMap();
+      Map<String, String> miMapa = new HashMap<>(); //convertimos el map también en generic, tenemos que poner el tipo de objeto tanto para la llave como para el valor
       miMapa.put("valor1", "juan"); //asociamos el valor juan a la key valor1
       miMapa.put("valor2", "Carla");
       miMapa.put("valor3", "Pepi");
+      miMapa.put("valor3", "Carlos"); //al repetirse la llave, se quedará el valor último que se le haya dado a la llave
       
       
-//      String elemento = (String ) miMapa.get("valor1");
-//      System.out.println("elemento = " + elemento);
+      String elementoMapa = miMapa.get("valor1"); //ya no hace falta hacer conversiones debido a que ya no son tipo object, si no tipo string
+//     System.out.println("elemento = " + elementoMapa);
       
-      //imprimir(miMapa.keySet()); //asi imprimimos todos las keys (no sale en orden)
-      //imprimir(miMapa.values()); //así imprimiría todos los valores
+      imprimir(miMapa.keySet()); //asi imprimimos todos las keys (no sale en orden)
+      imprimir(miMapa.values()); //así imprimiría todos los valores
       
     }
     
     public static void imprimir(Collection<String> coleccion) { //le indicamos el tipo de objeto
         
-        for(Object elemento: coleccion){
+        for(String elemento: coleccion){
             System.out.println("elemento = " + elemento);
         }
     }
