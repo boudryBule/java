c0b36f038f50c92f7de162bce75b17ef51b90c09
Juan Manuel Boudry León <buleboudry117@gmail.com>
2021-05-04 14:08:08 +0200

    Diferencia entre hash y list

--- Programacion/ApiColecciones/src/test/TestColecciones.java
+++ Programacion/ApiColecciones/src/test/TestColecciones.java
@@ -14,15 +14,37 @@ public class TestColecciones {
         miLista.add("Sabado");
         miLista.add("Domingo");
         
+        imprimir(miLista);
+        
 //           for(Object elemento: miLista) {
 //            System.out.println("elemento = " + elemento);
 //        }
         
-        //también se podría hacer así 
-        miLista.forEach(elemento -> {
+//        //también se podría hacer así 
+//        miLista.forEach(elemento -> {
+//            System.out.println("elemento = " + elemento);
+//        });
+        
+
+
+        //set no añade de forma ordenada, por lo cual es más rápido 
+        //en set no se permiten elementos duplicados, por lo que si repetimos un elemento, este no se volverá a mostrar 
+        Set miSet = new HashSet();
+        
+        miSet.add("lunes");
+        miSet.add("Martes");
+        miSet.add("Miercoles");
+        miSet.add("Jueves");
+        miSet.add("Viernes");
+        
+        imprimir(miSet);
+        
+    }
+    
+    public static void imprimir(Collection coleccion) { //va a recibir el tipo más genérico, la interfaz collection
+        
+        for(Object elemento: coleccion){
             System.out.println("elemento = " + elemento);
-        });
-        
-        
+        }
     }
 }
