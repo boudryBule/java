ec15b3b700b025b361e384b27f9dd034d01f06fc
Juan Manuel Boudry León <buleboudry117@gmail.com>
2021-05-05 11:00:06 +0200

    Escribir en un archivo

--- Programacion/ManejoArchivos/src/manejoarchivos/ManejoArchivos.java
+++ Programacion/ManejoArchivos/src/manejoarchivos/ManejoArchivos.java
@@ -18,5 +18,18 @@ public class ManejoArchivos {
         }
         
     }
+    
+    public static void escribirArchivo(String nombreArchivo, String contenido){
+        File archivo = new File(nombreArchivo); //así creamos el objeto de tipo file en memoria, pero no lo hemos guardado todavía en el disco duro
+        try {
+            PrintWriter salida = new PrintWriter(archivo); //abrimos el archivo 
+            salida.println(contenido); //asi escribimos en el archivo
+            salida.close(); //cuando llamamos al método close es cuando se crea el archivo en el disco duro
+            System.out.println("Se ha escrito al archivo");
+
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace(System.out); //imprimimos la excepcion
+        }
+    }
 }
  
--- Programacion/ManejoArchivos/src/test/TestManejoArchivos.java
+++ Programacion/ManejoArchivos/src/test/TestManejoArchivos.java
@@ -1,14 +1,20 @@
 
 package test;
 
-import static manejoarchivos.ManejoArchivos.crearArchivo;
+import static manejoarchivos.ManejoArchivos.*;
+
 
 
 public class TestManejoArchivos {
     public static void main(String[] args) {
         var nombreArchivo = "prueba.txt";
                         // "c:\\carpeta\\prueba.txt" doble diagonal en windows ya que  la primera diagonal es para escapar el siguiente caracter de diagonal
+                        // "/carpeta/prueba.txt" en linux o mac
+        //crearArchivo(nombreArchivo);
         
-        crearArchivo(nombreArchivo);
+        
+        
+        escribirArchivo(nombreArchivo, "Hola desde java");
     }
+    
 }
