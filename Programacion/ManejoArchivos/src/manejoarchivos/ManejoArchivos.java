2841fdccc286c45fa7b87a7e16ff98cb4f473634
Juan Manuel Boudry León <buleboudry117@gmail.com>
2021-05-05 10:21:06 +0200

    Manejo de archivos en java

---
+++ Programacion/ManejoArchivos/src/manejoarchivos/ManejoArchivos.java
@@ -0,0 +1,22 @@
+
+package manejoarchivos;
+
+//io significa input output así que es el paquete de entrada salida en java
+import java.io.*;
+
+
+public class ManejoArchivos {
+    public static void crearArchivo(String nombreArchivo) {
+        File archivo = new File(nombreArchivo); //así creamos el objeto de tipo file en memoria, pero no lo hemos guardado todavía en el disco duro
+        try {
+            PrintWriter salida = new PrintWriter(archivo); //abrimos el archivo 
+            salida.close(); //cuando llamamos al método close es cuando se crea el archivo en el disco duro
+            System.out.println("Se ha creado el archivo");
+            
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace(System.out); //imprimimos la excepcion
+        }
+        
+    }
+}
+ 
