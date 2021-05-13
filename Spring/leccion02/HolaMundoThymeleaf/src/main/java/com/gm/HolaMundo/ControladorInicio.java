0928fafcf2e030ad6f26c4b1438a1a41bd699a55
Juan Manuel Boudry León <buleboudry117@gmail.com>
2021-05-13 20:39:52 +0200

    Creación dominio Persona

--- Spring/leccion02/HolaMundoThymeleaf/src/main/java/com/gm/HolaMundo/ControladorInicio.java
+++ Spring/leccion02/HolaMundoThymeleaf/src/main/java/com/gm/HolaMundo/ControladorInicio.java
@@ -1,5 +1,6 @@
 package com.gm.HolaMundo;
 
+import com.gm.HolaMundo.domain.Persona;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -19,9 +20,19 @@ public class ControladorInicio {
     @GetMapping("/") //mapeamos a un path  para indicarle al navegador que es el método que se tiene que ejecutar
     public String inicio(Model model) { //con model vamos a agregar la información que queremos compartir con nuestra vista
         var mensaje = "Hola mundo con thymeleaf";
+        
+        
+        var persona = new Persona();
+        persona.setNombre("Juan");
+        persona.setApellido("Perez");
+        persona.setEmail("jperez@mail.com");
+        persona.setTelefono("554488");
+        
+        
         log.info("Ejecutando el controlador Spring MVC");
         model.addAttribute("mensaje", mensaje); //compartimos la llave y el valor de la variable mensaje
         model.addAttribute("saludo", saludo);
+        model.addAttribute("persona", persona);
         return "index";
     }
-}
+}           
