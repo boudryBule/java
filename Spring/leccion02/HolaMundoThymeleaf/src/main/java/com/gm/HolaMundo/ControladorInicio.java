3ea6ed96182fed7779e0ac4b2ff644c4240c2d59
Juan Manuel Boudry León <buleboudry117@gmail.com>
2021-05-14 11:15:13 +0200

    Creación lista de personas

--- Spring/leccion02/HolaMundoThymeleaf/src/main/java/com/gm/HolaMundo/ControladorInicio.java
+++ Spring/leccion02/HolaMundoThymeleaf/src/main/java/com/gm/HolaMundo/ControladorInicio.java
@@ -1,6 +1,9 @@
 package com.gm.HolaMundo;
 
 import com.gm.HolaMundo.domain.Persona;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -28,11 +31,27 @@ public class ControladorInicio {
         persona.setEmail("jperez@mail.com");
         persona.setTelefono("554488");
         
+        var persona2 = new Persona();
+        persona2.setNombre("Carla");
+        persona2.setApellido("Gomez");
+        persona2.setEmail("cgomez@mail.com");
+        persona2.setTelefono("112233");
+        
+//        List<Persona> personas = new ArrayList();
+//        personas.add(persona);
+//        personas.add(persona2);
+        
+//        también puede añadirse así: 
+
+        var personas = Arrays.asList(persona, persona2);
+        
+        
         
         log.info("Ejecutando el controlador Spring MVC");
         model.addAttribute("mensaje", mensaje); //compartimos la llave y el valor de la variable mensaje
         model.addAttribute("saludo", saludo);
-        model.addAttribute("persona", persona);
+        //model.addAttribute("persona", persona);
+        model.addAttribute("personas", personas); //vamos a compartir la lista de objetos de tipo persona
         return "index";
     }
 }           
