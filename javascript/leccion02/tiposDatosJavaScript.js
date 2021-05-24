ce1d1acff44f62911153614e6591a8c528e9d566
Juan Manuel Boudry León <buleboudry117@gmail.com>
2021-05-24 12:13:44 +0200

    más tipos de datos

--- javascript/leccion02/tiposDatosJavaScript.js
+++ javascript/leccion02/tiposDatosJavaScript.js
@@ -19,3 +19,49 @@ var objeto = {
 }
 
 console.log(typeof objeto);
+
+//tipo de dato boolean (se les conoce como bandera)
+
+var bandera = true;
+
+console.log(typeof bandera);
+
+
+//tipo de fato function
+
+function miFuncion(){}
+
+console.log(typeof miFuncion);
+
+//tipo Symbol
+
+var simbolo = Symbol("mi simbolo"); //crear un valor unico de una variable
+
+console.log(typeof simbolo);
+
+
+//Tipo clase es una funcción
+
+class Persona{
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+console.log(typeof Persona);
+
+
+//tipo  undefined
+
+var x;
+console.log(x); //al no ponerle ningún valor, la trata como undefined
+
+x = undefined;
+
+console.log(x);
+//null = ausencia de valor
+
+var y  = null;
+
+console.log(y);
