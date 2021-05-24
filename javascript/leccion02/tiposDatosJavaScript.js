8523fb2de65db42b1967346b0372733c7fc509ab
Juan Manuel Boudry León <buleboudry117@gmail.com>
2021-05-24 11:21:23 +0200

    tipos de datos más importantes en javascript

---
+++ javascript/leccion02/tiposDatosJavaScript.js
@@ -0,0 +1,18 @@
+//tipo de dato string
+var nombre = "Carlos";
+console.log(nombre);
+
+
+//tipo de dato numerico
+var numero = 1000;
+console.log(numero);
+
+//tipo de dato object
+
+var objeto = {
+    nombre : "Juan",
+    apellido : "Perez", 
+    telefono : "20202022"
+}
+
+console.log(objeto);
