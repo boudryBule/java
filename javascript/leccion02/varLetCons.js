8c56749febf5654ed264bef69105734447d0cb25
Juan Manuel Boudry León <buleboudry117@gmail.com>
2021-05-24 19:01:45 +0200

    uso de var, const y let

---
+++ javascript/leccion02/varLetCons.js
@@ -0,0 +1,10 @@
+//ya no se recomienda usar var para declarar variables en javaScript
+
+nombre = "Juan"; //se puede declarar sin necesidad de usar palabra reservada (no es una buena práctica)
+
+console.log(nombre);
+
+let nombre1 = "Juan"; //así sería una buena práctica
+
+const apellido ="Perez" //una vez asignado un valor, no se puede modificar 
+
