e5f273fd0958196473c1858673a177bebbf50dff
Juan Manuel Boudry León <buleboudry117@gmail.com>
2021-05-25 11:23:29 +0200

    operadores aritméticos en javaScript

---
+++ javascript/leccion03/03-01-operadoresJavaScript.js
@@ -0,0 +1,20 @@
+let a = 3, b = 2;
+let z = a + b;
+console.log("Resultado de la suma: "+z);
+
+z = a -b;
+console.log("Resultado de la resta: "+z);
+
+z  = a *b;
+console.log("Resultado de la multiplicacion: "+z);
+
+z = a /b;
+console.log("Resultado de la division: "+z);
+
+z = a%b; 
+
+console.log("Resultado del modulo  : "+z);
+
+z = a ** b;
+
+console.log("Resultado del operador exponente: "+z);
