a446500aaff460bcf2ac6aa9c90d02fdd05f2ea5
Juan Manuel Boudry León <buleboudry117@gmail.com>
2021-05-25 12:03:47 +0200

    post y pre incrementos/decrementos

--- javascript/leccion03/03-01-operadoresJavaScript.js
+++ javascript/leccion03/03-01-operadoresJavaScript.js
@@ -18,3 +18,28 @@ console.log("Resultado del modulo  : "+z);
 z = a ** b;
 
 console.log("Resultado del operador exponente: "+z);
+
+//incremento
+
+//preincremento 
+
+z = ++a;
+console.log(a);
+console.log(z);
+
+//post incremento
+z = b++; //primero se asigna el valor y después se incrementa
+console.log(b);
+console.log(z);
+
+//decremento
+//pre decremento
+z = --a;
+console.log(a);
+console.log(z);
+
+//post decremento
+
+z = b--;
+console.log(b);
+console.log(z);
